36f107b99d45b4c588257426616c51e203eb4f1e
mdimtias <developertanbir@gmail.com>
2023-01-14 17:00:48 +0600

    Simple Login Registration System add

--- Client/src/routes/Router.js
+++ Client/src/routes/Router.js
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main/Main";
 import ErrorPage from "../Pages/ErrorPage/ErrorPage";
 import Home from "../Pages/Home/Home/Home";
+import LogIn from "../Pages/LogIn/LogIn/LogIn";
+import Register from "../Pages/LogIn/Register/Register";
 
 export const router = createBrowserRouter([
     {
@@ -12,6 +14,14 @@ export const router = createBrowserRouter([
             {
                 path: "/",
                 element: <Home></Home>
+            }, 
+            {
+                path: "/register",
+                element: <Register></Register>
+            }, 
+            {
+                path: "/login",
+                element: <LogIn></LogIn>
             }, 
         ]
     }
