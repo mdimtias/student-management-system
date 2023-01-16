9b3f6874323bb49fcc25f6c82ddfc8c6354edfa2
muradxkira <mahfujur797@gmail.com>
2023-01-17 01:01:12 +0600

    student skill development resouces added and nav fixed

--- Client/src/Pages/StudentProfile/StudentProfile.tsx
+++ Client/src/Pages/StudentProfile/StudentProfile.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const StudentProfile = () => {
     return (
-        <div className='flex flex-col md:flex-row my-5 mx-2 md:mx-0'> 
-            <div className='w-full md:flex md:flex-col  md:w-1/5 p-5 md:ml-10 md:mr-5 shadow-2xl hidden rounded-2xl h-fit	'>
+        <div className='flex flex-col md:flex-row pt-20 mx-2 md:mx-0'> 
+            <div className='w-full md:flex md:flex-col  md:w-1/5 p-5 md:ml-5 md:mr-5 shadow-2xl hidden rounded-2xl h-fit	'>
                 <img src="https://img.freepik.com/premium-vector/man-profile-cartoon_18591-58482.jpg?w=740" alt="" />
                 <h1 className='font-semibold text-center'>Will byers</h1>
                 <h2 className='text-sm text-center font-medium'>Student</h2>
@@ -28,7 +28,7 @@ const StudentProfile = () => {
                 {/* cards 1  start here*/}
 
                 <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-full gap-5 my-5'>
-                    <div className=' shadow-xl hover:shadow-2xl rounded-2xl ease-in-out duration-500 h-fit'>
+                    <div className=' shadow-xl hover:border-blue-500 hover:shadow-2xl rounded-2xl ease-in-out duration-500 h-96'>
                         <img className='rounded-t-2xl' src="https://img.freepik.com/free-photo/sad-female-workaholic-keeps-hands-chin-busy-making-project-work-studies-papers-wears-elegant-white-shirt-sits-desktop-unknown-people-stretch-hands-with-notes-alarm-clock-smartphone_273609-26174.jpg?w=740&t=st=1673601887~exp=1673602487~hmac=a5c57ce73a8fbd5954d51baa8982f0904d52a009f9fbd63229976b086a310798" alt="" />
 
                         <div className='px-5 pb-5 pt-2'>
@@ -54,11 +54,11 @@ const StudentProfile = () => {
 
                     </div>
 
-                    <div className=' shadow-xl hover:shadow-2xl rounded-2xl ease-in-out duration-500 h-fit'>
+                    <div className=' shadow-xl hover:shadow-2xl rounded-2xl ease-in-out duration-500 h-96'>
                         <img className='rounded-t-2xl' src="https://img.freepik.com/free-photo/top-view-smilimg-business-team-sitting-round-table-white-background-concept-successful-transaction_155003-16562.jpg?w=740&t=st=1673602201~exp=1673602801~hmac=096c11eb5f64fc9ecbb2fc37fc80c527c12e880361ba0d49ce5da8d2356f4e41" alt="" />
 
                         <div className='px-5 pb-5 pt-2'>
-                            <h1 className='font-semibold'>Effective communication and collaboration</h1>
+                            <h1 className='font-semibold'>Effective communication </h1>
 
                             <div className='flex mt-2 items-center'>
                                 <img className='w-10 rounded-full mr-5' src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRY5ch3pSRAQPn_rmxgwdwPhnrrf24dqRl3VmZo5xIjpHnIusVI" alt="" />
@@ -84,7 +84,7 @@ const StudentProfile = () => {
 
 
 
-                    <div className=' shadow-xl hover:shadow-2xl rounded-2xl ease-in-out duration-500 h-fit'>
+                    <div className=' shadow-xl hover:shadow-2xl rounded-2xl ease-in-out duration-500 h-96'>
                         <img className='rounded-t-2xl' src="https://img.freepik.com/free-photo/elderly-man-with-bulbs_1134-476.jpg?w=740&t=st=1673602533~exp=1673603133~hmac=553679b970f6b29ef8444e7b158d33f4f398165201edaa9b91a269d735ba0578" alt="" />
 
                         <div className='px-5 pb-5 pt-2'>
@@ -110,7 +110,7 @@ const StudentProfile = () => {
 
                     </div>
 
-                    <div className=' shadow-xl hover:shadow-2xl rounded-2xl ease-in-out duration-500 h-fit'>
+                    <div className=' shadow-xl hover:shadow-2xl rounded-2xl ease-in-out duration-500 h-96'>
                         <img className='rounded-t-2xl' src="https://img.freepik.com/free-photo/elderly-man-with-bulbs_1134-476.jpg?w=740&t=st=1673602533~exp=1673603133~hmac=553679b970f6b29ef8444e7b158d33f4f398165201edaa9b91a269d735ba0578" alt="" />
 
                         <div className='px-5 pb-5 pt-2'>
@@ -136,11 +136,11 @@ const StudentProfile = () => {
 
                     </div>
 
-                    <div className=' shadow-xl hover:shadow-2xl rounded-2xl ease-in-out duration-500 h-fit'>
+                    <div className=' shadow-xl hover:shadow-2xl rounded-2xl ease-in-out duration-500 h-96'>
                         <img className='rounded-t-2xl' src="https://img.freepik.com/free-photo/top-view-smilimg-business-team-sitting-round-table-white-background-concept-successful-transaction_155003-16562.jpg?w=740&t=st=1673602201~exp=1673602801~hmac=096c11eb5f64fc9ecbb2fc37fc80c527c12e880361ba0d49ce5da8d2356f4e41" alt="" />
 
                         <div className='px-5 pb-5 pt-2'>
-                            <h1 className='font-semibold'>Effective communication and collaboration</h1>
+                            <h1 className='font-semibold'>Effective communication </h1>
 
                             <div className='flex mt-2 items-center'>
                                 <img className='w-10 rounded-full mr-5' src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRY5ch3pSRAQPn_rmxgwdwPhnrrf24dqRl3VmZo5xIjpHnIusVI" alt="" />
@@ -163,7 +163,7 @@ const StudentProfile = () => {
                     </div>
 
 
-                    <div className=' shadow-xl hover:shadow-2xl rounded-2xl ease-in-out duration-500 h-fit'>
+                    <div className=' shadow-xl hover:shadow-2xl rounded-2xl ease-in-out duration-500 h-96'>
                         <img className='rounded-t-2xl' src="https://img.freepik.com/free-photo/sad-female-workaholic-keeps-hands-chin-busy-making-project-work-studies-papers-wears-elegant-white-shirt-sits-desktop-unknown-people-stretch-hands-with-notes-alarm-clock-smartphone_273609-26174.jpg?w=740&t=st=1673601887~exp=1673602487~hmac=a5c57ce73a8fbd5954d51baa8982f0904d52a009f9fbd63229976b086a310798" alt="" />
 
                         <div className='px-5 pb-5 pt-2'>
